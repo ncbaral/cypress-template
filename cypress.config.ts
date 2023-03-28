47ce56f34388b504f9fd59de49bb3e78c77bbb8c
Nirmal Baral <nirmal.baral@gmail.com>
2023-03-28 23:40:24 +0100

    add basic cypress template

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://example.cypress.io',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+    },
+  },
+  video: false,
+});
