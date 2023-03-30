e3af6d590a06cfbc82609a88596d05bb87cf5931
Nirmal Baral <nirmal.baral@gmail.com>
2023-03-30 21:23:33 +0100

    Cypress cucumber template

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,31 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://example.cypress.io',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-
-    },
-  },
+export default defineConfig({
+  defaultCommandTimeout: 5000,
   video: false,
+  e2e: {
+    baseUrl: "https://www.duckduckgo.com",
+    specPattern: "**/*.feature",
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+    env: {
+      omitFiltered: true,
+      filterSpecs: true
+    },
+    fixturesFolder: false,
+  },
 });
